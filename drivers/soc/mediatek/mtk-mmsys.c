171308890609c1499d1aad093323f4abd87ec43a
rva3 <rva333@protonmail.com>
2026-03-13 13:18:27 +0200

    soc: mediatek: mmsys: add mt6572 support
    XXX: fixme
    Signed-off-by: rva3 <rva333@protonmail.com>

--- drivers/soc/mediatek/mtk-mmsys.c
+++ drivers/soc/mediatek/mtk-mmsys.c
@@ -14,6 +14,7 @@
 #include <linux/soc/mediatek/mtk-mmsys.h>
 
 #include "mtk-mmsys.h"
+#include "mt6572-mmsys.h"
 #include "mt8167-mmsys.h"
 #include "mt8173-mmsys.h"
 #include "mt8183-mmsys.h"
@@ -35,6 +36,12 @@ static const struct mtk_mmsys_driver_data mt2712_mmsys_driver_data = {
 	.clk_driver = "clk-mt2712-mm",
 	.routes = mmsys_default_routing_table,
 	.num_routes = ARRAY_SIZE(mmsys_default_routing_table),
+};
+
+static const struct mtk_mmsys_driver_data mt6572_mmsys_driver_data = {
+	.clk_driver = "clk-mt6572-mm",
+	.routes = mt6572_mmsys_routing_table,
+	.num_routes = ARRAY_SIZE(mt6572_mmsys_routing_table),
 };
 
 static const struct mtk_mmsys_driver_data mt6779_mmsys_driver_data = {
@@ -458,6 +465,7 @@ static void mtk_mmsys_remove(struct platform_device *pdev)
 static const struct of_device_id of_match_mtk_mmsys[] = {
 	{ .compatible = "mediatek,mt2701-mmsys", .data = &mt2701_mmsys_driver_data },
 	{ .compatible = "mediatek,mt2712-mmsys", .data = &mt2712_mmsys_driver_data },
+	{ .compatible = "mediatek,mt6572-mmsys", .data = &mt6572_mmsys_driver_data },
 	{ .compatible = "mediatek,mt6779-mmsys", .data = &mt6779_mmsys_driver_data },
 	{ .compatible = "mediatek,mt6795-mmsys", .data = &mt6795_mmsys_driver_data },
 	{ .compatible = "mediatek,mt6797-mmsys", .data = &mt6797_mmsys_driver_data },
