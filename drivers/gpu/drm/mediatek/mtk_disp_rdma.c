35e1d7707be5441b8326c6065db71a26c2b1ab16
rva3 <rva333@protonmail.com>
2026-03-22 13:29:54 +0200

    drm: mediatek: disp_rdma: use function pointer for the fmt_convert
    RDMA uses different values for memory formats. Currently, these are
    hardcoded using macros.
    
    Move the format conversion logic into function pointer instead of raw
    call. This allows other SoCs to provide their own mapping logic without
    breaking others.
    
    Also drop unused parameter in the rdma_fmt_convert method.
    
    Signed-off-by: rva3 <rva333@protonmail.com>

--- drivers/gpu/drm/mediatek/mtk_disp_rdma.c
+++ drivers/gpu/drm/mediatek/mtk_disp_rdma.c
@@ -36,6 +36,10 @@
 #define DISP_REG_RDMA_SIZE_CON_1		0x0018
 #define DISP_REG_RDMA_TARGET_LINE		0x001c
 #define DISP_RDMA_MEM_CON			0x0024
+
+#define MEM_MODE_INPUT_FORMAT_RGB565_MT65XX		(0x004 << 4)
+#define MEM_MODE_INPUT_FORMAT_RGB888_MT65XX		(0x008 << 4)
+
 #define MEM_MODE_INPUT_FORMAT_RGB565			(0x000 << 4)
 #define MEM_MODE_INPUT_FORMAT_RGB888			(0x001 << 4)
 #define MEM_MODE_INPUT_FORMAT_RGBA8888			(0x002 << 4)
@@ -74,6 +78,7 @@ struct mtk_disp_rdma_data {
 	size_t num_formats;
 	u32 size_con0;
 	u32 size_con1;
+	unsigned int (*fmt_convert)(unsigned int fmt);
 };
 
 /*
@@ -215,8 +220,43 @@ void mtk_rdma_config(struct device *dev, unsigned int width,
 	mtk_ddp_write(cmdq_pkt, reg, &rdma->cmdq_reg, rdma->regs, DISP_REG_RDMA_FIFO_CON);
 }
 
-static unsigned int rdma_fmt_convert(struct mtk_disp_rdma *rdma,
-				     unsigned int fmt)
+static unsigned int rdma_fmt_convert_mt65xx(unsigned int fmt)
+{
+	/* The return value in switch "MEM_MODE_INPUT_FORMAT_XXX"
+	 * is defined in mediatek HW data sheet.
+	 * The alphabet order in XXX is no relation to data
+	 * arrangement in memory.
+	 */
+	switch (fmt) {
+	default:
+	case DRM_FORMAT_RGB565:
+		return MEM_MODE_INPUT_FORMAT_RGB565_MT65XX;
+	case DRM_FORMAT_BGR565:
+		return MEM_MODE_INPUT_FORMAT_RGB565_MT65XX | MEM_MODE_INPUT_SWAP;
+	case DRM_FORMAT_RGB888:
+		return MEM_MODE_INPUT_FORMAT_RGB888_MT65XX;
+	case DRM_FORMAT_BGR888:
+		return MEM_MODE_INPUT_FORMAT_RGB888_MT65XX | MEM_MODE_INPUT_SWAP;
+	case DRM_FORMAT_RGBX8888:
+	case DRM_FORMAT_RGBA8888:
+		return MEM_MODE_INPUT_FORMAT_ARGB8888;
+	case DRM_FORMAT_BGRX8888:
+	case DRM_FORMAT_BGRA8888:
+		return MEM_MODE_INPUT_FORMAT_ARGB8888 | MEM_MODE_INPUT_SWAP;
+	case DRM_FORMAT_XRGB8888:
+	case DRM_FORMAT_ARGB8888:
+		return MEM_MODE_INPUT_FORMAT_RGBA8888;
+	case DRM_FORMAT_XBGR8888:
+	case DRM_FORMAT_ABGR8888:
+		return MEM_MODE_INPUT_FORMAT_RGBA8888 | MEM_MODE_INPUT_SWAP;
+	case DRM_FORMAT_UYVY:
+		return MEM_MODE_INPUT_FORMAT_UYVY;
+	case DRM_FORMAT_YUYV:
+		return MEM_MODE_INPUT_FORMAT_YUYV;
+	}
+}
+
+static unsigned int rdma_fmt_convert(unsigned int fmt)
 {
 	/* The return value in switch "MEM_MODE_INPUT_FORMAT_XXX"
 	 * is defined in mediatek HW data sheet.
@@ -268,7 +308,7 @@ void mtk_rdma_layer_config(struct device *dev, unsigned int idx,
 	unsigned int fmt = pending->format;
 	unsigned int con;
 
-	con = rdma_fmt_convert(rdma, fmt);
+	con = rdma->data->fmt_convert(fmt);
 	mtk_ddp_write_relaxed(cmdq_pkt, con, &rdma->cmdq_reg, rdma->regs, DISP_RDMA_MEM_CON);
 
 	if (fmt == DRM_FORMAT_UYVY || fmt == DRM_FORMAT_YUYV) {
@@ -384,6 +424,7 @@ static const struct mtk_disp_rdma_data mt2701_rdma_driver_data = {
 	.num_formats = ARRAY_SIZE(mt8173_formats),
 	.size_con0 = 0xfff,
 	.size_con1 = 0xfffff,
+	.fmt_convert = rdma_fmt_convert,
 };
 
 static const struct mtk_disp_rdma_data mt8173_rdma_driver_data = {
@@ -392,6 +433,7 @@ static const struct mtk_disp_rdma_data mt8173_rdma_driver_data = {
 	.num_formats = ARRAY_SIZE(mt8173_formats),
 	.size_con0 = 0xfff,
 	.size_con1 = 0xfffff,
+	.fmt_convert = rdma_fmt_convert,
 };
 
 static const struct mtk_disp_rdma_data mt8183_rdma_driver_data = {
@@ -400,6 +442,7 @@ static const struct mtk_disp_rdma_data mt8183_rdma_driver_data = {
 	.num_formats = ARRAY_SIZE(mt8173_formats),
 	.size_con0 = 0xfff,
 	.size_con1 = 0xfffff,
+	.fmt_convert = rdma_fmt_convert,
 };
 
 static const struct mtk_disp_rdma_data mt8195_rdma_driver_data = {
@@ -408,6 +451,7 @@ static const struct mtk_disp_rdma_data mt8195_rdma_driver_data = {
 	.num_formats = ARRAY_SIZE(mt8173_formats),
 	.size_con0 = 0xfff,
 	.size_con1 = 0xfffff,
+	.fmt_convert = rdma_fmt_convert,
 };
 
 static const struct of_device_id mtk_disp_rdma_driver_dt_match[] = {
